3e2e0ef548f762a700e732f0b95d819791b07af5
JuneSeo <magpieprinc@gmail.com>
2023-02-13 11:02:00 +0900

    save - 1

--- src/main/kotlin/com/example/mitalk/global/redis/util/RedisUtils.kt
+++ src/main/kotlin/com/example/mitalk/global/redis/util/RedisUtils.kt
@@ -1,59 +1,55 @@
 package com.example.mitalk.global.redis.util
 
-import org.springframework.data.domain.Range
-import org.springframework.data.redis.core.ReactiveZSetOperations
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.core.ZSetOperations
 import org.springframework.stereotype.Component
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
-
 
 @Component
 class RedisUtils(
         private val redisTemplate: RedisTemplate<String, Any>
 ) {
 
-    /**
-     * @desc: Sorted Set 삭제.
-     */
-    fun delete(key: String): Boolean {
-        return redisTemplate.delete(key)
+    companion object {
+        const val KEY = "CustomerQueue"
+        const val MAX_SIZE = 50
     }
 
-    /**
-     * @desc: Sorted Set 조회
-     */
-    fun getValue(key: String): Any? {
-        return redisTemplate.opsForValue().get(key)
-    }
+//    /**
+//     * @desc: Sorted Set 삭제.
+//     */
+//    fun delete(key: String): Boolean {
+//        return redisTemplate.delete(key)
+//    }
+//
+//    /**
+//     * @desc: Sorted Set 조회
+//     */
+//    fun getValue(key: String): Any? {
+//        return redisTemplate.opsForValue().get(key)
+//    }
 
     /**
      * @desc: RedisTemplate에 SortedSet 초기화.
      */
-    fun opsForZSet(): ZSetOperations<String, Any> {
-        return redisTemplate.opsForZSet()
-    }
+    fun opsForZSet() = redisTemplate.opsForZSet()
 
     /**
      * @desc: Sorted Set 자료형 사이즈
      */
-    fun zCard(str: String): Long? {
-        val z: ZSetOperations<String, Any> = redisTemplate.opsForZSet()
-        return z.size(str)
-    }
+    fun zSize() = opsForZSet().size(KEY) ?: 0
+
+    // 꽉참 = true, 비어있음 = false
+    fun isQueueFull() = zSize() > MAX_SIZE
 
     /**
      * @desc: Sorted Set 자료형 start ~ end 까지 조회.
      */
-    fun zRange(key: String, start: Long, end: Long): MutableSet<Any>? {
-        return opsForZSet().range(key, start, end)
-    }
-
+    fun zRange(start: Long, end: Long) = opsForZSet().range(KEY, start, end) ?: 0
     /**
      * @desc: Sorted Set 자료형 Value의 현재위치 조회.
      */
-    fun getzRank(key: String, value: String): Long? {
-        return opsForZSet().rank(key, value!!)
-    }
+    fun zRank(value: String) = opsForZSet().rank(KEY, value)
+
+    fun zAdd(value: Any) = opsForZSet().add(KEY, value, System.currentTimeMillis().toDouble())
+
+    fun zDelete(value: Any) = opsForZSet().remove(KEY, value)
 }
--- src/main/kotlin/com/example/mitalk/global/socket/handler/SocketHandler.kt
+++ src/main/kotlin/com/example/mitalk/global/socket/handler/SocketHandler.kt
@@ -1,5 +1,6 @@
 package com.example.mitalk.global.socket.handler
 
+import com.example.mitalk.global.redis.util.RedisUtils
 import com.example.mitalk.global.socket.ChatService
 import com.example.mitalk.global.socket.dto.ChatMessage
 import com.example.mitalk.global.socket.dto.Role
@@ -15,21 +16,38 @@ import org.springframework.web.socket.handler.TextWebSocketHandler
 class SocketHandler(
     val mapper: ObjectMapper,
     val chatService: ChatService,
+    val redisUtils: RedisUtils
 ) : TextWebSocketHandler() {
-    companion object {
-        private var list: MutableList<WebSocketSession> = arrayListOf()
-    }
+
+    //session connection 감지
     override fun afterConnectionEstablished(session: WebSocketSession) {
-        session.attributes.forEach {
-            println(session.attributes)
+        //TODO("attribute 검사")
+
+        //고객인 경우 -> 대기열 입력
+        if (!redisUtils.isQueueFull()) {
+            redisUtils.zAdd(session)
+            println("$session 큐 입력")
+
+            //TODO("안드로이드 한테 큐접속 했다 보내기 + 현재 rank")
+            //session.sendMessage()
+        } else {
+            println("$session 큐 입력 실패")
+            //TODO("안드한테 실패했다고 보내기")
         }
-        list.add(session)
-        println("$session 클라이언트 접속")
+
+        //
+
     }
 
+    //session 종료 감지
     override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
-        println("$session 클라이언트 접속 해제")
-        list.remove(session)
+        val removeCount = redisUtils.zDelete(session)
+        println("queue에서 session $removeCount 개가 정상적으로 제거되었습니다.")
+
+        println("$session 클라이언트 접속 해제 + $status")
+
+        //TODO("안드한테 접속해제됬다고 보내기")
+
     }
 
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
@@ -38,7 +56,7 @@ class SocketHandler(
         val chatMessage = mapper.readValue(payload, ChatMessage::class.java)
         val room = chatService.findRoomById(chatMessage.roomId)
         if(room != null) {
-            room.handleActions(session, chatMessage, chatService)
+//            room.handleActions(session, chatMessage, chatService)
         } else println("room was not exist")
     }
 
