cb47ee99fcc7f9567b229fd4ba0f002183a8594a
Seungwon <jswa7308@naver.com>
2023-02-14 14:29:45 +0900

    빈 오류 해결

--- src/main/kotlin/com/example/mitalk/MitalkApplication.kt
+++ src/main/kotlin/com/example/mitalk/MitalkApplication.kt
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @ConfigurationPropertiesScan
 @EnableScheduling
+@ComponentScan("com.example.mitalk.domain.customer.domain.repository")tatus
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class MitalkApplication
 
