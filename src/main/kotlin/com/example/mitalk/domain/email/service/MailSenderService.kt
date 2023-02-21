8a33e1daea9a6b6311a4a39f5a194a5b3636927f
Seungwon <jswa7308@naver.com>
2023-02-21 14:16:53 +0900

    application.yml 수정

--- src/main/kotlin/com/example/mitalk/domain/email/service/MailSenderService.kt
+++ src/main/kotlin/com/example/mitalk/domain/email/service/MailSenderService.kt
@@ -11,6 +11,8 @@ import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.thymeleaf.context.Context
+import org.thymeleaf.spring5.SpringTemplateEngine
 import java.time.LocalDateTime
 import java.util.*
 import javax.mail.internet.MimeMessage
@@ -18,10 +20,7 @@ import javax.mail.internet.MimeMessage
 
 @Service
 @EnableAsync
-class MailSenderService(
-        private val mailSender: JavaMailSender,
-        private val recordRepository: RecordRepository
-) {
+class MailSenderService(private val mailSender: JavaMailSender, private val recordRepository: RecordRepository, private val templateEngine: SpringTemplateEngine) {
     @Async
     @Transactional(rollbackFor = [Exception::class])
     fun execute(emailSentDto: EmailSentDto) {
@@ -73,94 +72,50 @@ class MailSenderService(
         var list = ""
         for (it in top1Record.messageRecords) {// 고객 이냐 상담원이냐 여부
             for (i in 0 until it.dataMap.size) {
+
                 if (it.sender.name == Role.CUSTOMER.name) {
-                    println(it.dataMap[i].message)
-                    if(it.dataMap[i].message.contains("https://mitalk-s3.s3.ap-northeast-2.amazonaws.com/")) {
-                        list += "<img src=https://mitalk-s3.s3.ap-northeast-2.amazonaws.com/eb504148-d31d-4fc3-8719-68b0618d4eca-image%253A61.jpeg>"
-//                        list += ("<p style=\"text-align: left; font-size: 15px\"><b>고객</b><br>\n" +
-//                                "                    <image src=${it.dataMap[i].message}\n" +
-//                                "                     style=\"float: left; width: 150px; margin-bottom: 10px\">\n" +
-//                                "                     <p style=\"display: flex; font-size: 12px; position: absolute; width: 56px;height: 21px;left: 230px;top: 580px\">${it.dataMap[i].localDateTime}</p>\n" +
-//                                "                </p>")
+
+                    if (checkS3URL(it.dataMap[i].message)) { // 이미지나 파일 URL
+                        val extensionName = extractExtensionName(it.dataMap[i].message)
                     } else {
                         list += ("        <p style=\"text-align: left;\"><b>고객</b> : ${it.dataMap[i].message} ${it.dataMap[i].localDateTime}</p>\n")
                     }
                 } else if (it.sender.name == Role.COUNSELLOR.name) {
-                    list += ("        <p style=\"text-align: right;\"><b>상담사</b> : ${it.dataMap[i].message} ${it.dataMap[i].localDateTime}</p>\n")
+
+                    if (checkS3URL(it.dataMap[i].message)) {
+
+                    } else {
+                        list += ("        <p style=\"text-align: left;\"><b>상담사</b> : ${it.dataMap[i].message} ${it.dataMap[i].localDateTime}</p>\n")
+                    }
                 }
             }
         }
-        return "<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                "    <title>Document</title>\n" +
-                "</head>\n" +
-                "<body style=\"background-color: rgb(231, 231, 231)\">\n" +
-                "    <h2>상담기록 안내</h2><hr><br>\n" +
-                "    <p>안녕하세요 MiTalk팀의 전승원 상담사 입니다.</p>\n" +
-                "    <table border=\"1\" style=\"text-align: center; border-color: rgb(109, 109, 109); border-width: 3px; border-style: solid; border-collapse: collapse;\">\n" +
-                "        <tr>\n" +
-                "            <td style=\"background-color: rgb(83, 83, 83); color: rgb(223, 221, 221); border-style: none;\">상담 내용</td>\n" +
-                "            <td>${type}</td>\n" +
-                "        </tr>\n" +
-                "        <tr>\n" +
-                "            <td style=\"background-color: rgb(83, 83, 83);  color: rgb(223, 221, 221);\">상담 일시</td>\n" +
-                "            <td>${date}</td>\n" +
-                "        </tr>\n" +
-                "        <tr>\n" +
-                "            <td style=\"background-color: rgb(83, 83, 83);  color: rgb(223, 221, 221);\">채팅 내용</td>\n" +
-                "            <td>\n" +
-                                    list
-                "            </td>\n" +
-                "        </tr>\n" +
-                "    </table>\n" +
-                "</body>\n" +
-                "</html>"
-
+        return ""
     }
 
 
     private fun getRecordTemplate2(): String {
+        val context = Context()
+        val html: String = templateEngine.process("test", context)
         var text = "        <p style=\"text-align: left;\"><b>고객</b> : {메시지} {시간}</p>\n"
         var list = ""
         for (i in 0..3) {
-            if(i == 1) {
+            if (i == 1) {
                 list += ("        <p style=\"text-align: right; color: rgb(98, 204, 204);\"><b>고객</b> : ${"안녕"} ${LocalDateTime.now()}</p>\n")
             }
             list += ("        <p style=\"text-align: left; color: rgb(98, 204, 204);\"><b>고객</b> : ${"안녕"} ${LocalDateTime.now()}</p>\n")
         }
-        return "<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                "    <title>Document</title>\n" +
-                "</head>\n" +
-                "<body style=\"background-color: rgb(231, 231, 231)\">\n" +
-                "    <h2>상담기록 안내</h2><hr><br>\n" +
-                "    <p>안녕하세요 MiTalk팀의 전승원 상담사 입니다.</p>\n" +
-                "    <table border=\"1\" style=\"text-align: center; border-color: rgb(146, 145, 145); border-width: 3px; border-style: solid;\">\n" +
-                "        <tr>\n" +
-                "            <td style=\"background-color: rgb(83, 83, 83); color: rgb(223, 221, 221);\">상담 내용</td>\n" +
-                "            <td>버그 관련</td>\n" +
-                "        </tr>\n" +
-                "        <tr>\n" +
-                "            <td style=\"background-color: rgb(83, 83, 83);  color: rgb(223, 221, 221);\">상담 일시</td>\n" +
-                "            <td>2023-02-20 19:55:06 ~ 2023-02-21 19:55:06</td>\n" +
-                "        </tr>\n" +
-                "        <tr>\n" +
-                "            <td style=\"background-color: rgb(83, 83, 83);  color: rgb(223, 221, 221);\">채팅 내용</td>\n" +
-                "            <td>\n" +
-                "                <p style=\"text-align: left; font-size: 15px;\"><b>고객</b> : {메시지} {시간}</p> <p style=\"text-align: right; font-size: 15px;\"><b>상담사</b> : {메시지} {시간}</p>\n" +
-                "                <p style=\"text-align: left; font-size: 15px\"><b>고객</b> : {메시지} {시간}</p> <p style=\"text-align: right; font-size: 15px;\"><b>상담사</b> : {메시지} {시간}</p>\n" +
-                "            </td>\n" +
-                "        </tr>\n" +
-                "    </table>\n" +
-                "</body>\n" +
-                "</html>"
+        return html
     }
+
+    private fun extractExtensionName(fileName: String): String {
+        val index = fileName.lastIndexOf(".")
+
+        if (index > 0) {
+            return fileName.substring(index + 1)
+        }
+        return ""
+    }
+
+    private fun checkS3URL(url: String): Boolean = url.contains("https://mitalk-s3.s3.ap-northeast-2.amazonaws.com/")
 }
